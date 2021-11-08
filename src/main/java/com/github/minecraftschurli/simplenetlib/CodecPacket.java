900012533ee02f4d1f1fa5b94995e1e9a431c6b9
Minecraftschurli <minecraftschurli@gmail.com>
2021-11-08 20:51:16 +0100

    move everything except textures to datagen

--- src/main/java/com/github/minecraftschurli/simplenetlib/CodecPacket.java
+++ src/main/java/com/github/minecraftschurli/simplenetlib/CodecPacket.java
@@ -2,10 +2,9 @@ package com.github.minecraftschurli.simplenetlib;
 
 import com.mojang.serialization.Codec;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraftforge.fmllegacy.network.NetworkEvent;
 
 public abstract class CodecPacket<T> implements IPacket {
-    private final T data;
+    protected final T data;
 
     public CodecPacket(T data) {
         this.data = data;
