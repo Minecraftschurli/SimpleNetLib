01aad36de9b633ee6f708f16c5232e1912a5d8c5
Minecraftschurli <minecraftschurli@gmail.com>
2021-12-11 00:41:06 +0100

    doc

--- src/main/java/com/github/minecraftschurli/simplenetlib/CodecPacket.java
+++ src/main/java/com/github/minecraftschurli/simplenetlib/CodecPacket.java
@@ -3,13 +3,27 @@ package com.github.minecraftschurli.simplenetlib;
 import com.mojang.serialization.Codec;
 import net.minecraft.network.FriendlyByteBuf;
 
+/**
+ * Abstract base class for a Packet using a codec to encode and decode the data.
+ *
+ * @param <T> the datatype of the data being sent.
+ */
 public abstract class CodecPacket<T> implements IPacket {
     protected final T data;
 
+    /**
+     * Constructor accepting the data to send.
+     *
+     * @param data the data to send.
+     */
     public CodecPacket(T data) {
         this.data = data;
     }
 
+    /**
+     * Constructor for deserialization.<br>
+     * Subclasses must have this constructor present.
+     */
     public CodecPacket(FriendlyByteBuf buf) {
         this.data = buf.readWithCodec(this.getCodec());
     }
@@ -19,5 +33,10 @@ public abstract class CodecPacket<T> implements IPacket {
         buf.writeWithCodec(this.getCodec(), this.data);
     }
 
+    /**
+     * Implement this method and return the codec to encode and decode the data.
+     *
+     * @return the codec to encode and decode the data.
+     */
     protected abstract Codec<T> getCodec();
 }
